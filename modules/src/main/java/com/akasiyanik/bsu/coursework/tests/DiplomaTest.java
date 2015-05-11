ea52d90210db1b494be246a17d6b93c6627199dc
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2015-05-12 01:55:46 +0300

    Added time watchers

--- modules/src/main/java/com/akasiyanik/bsu/coursework/tests/DiplomaTest.java
+++ modules/src/main/java/com/akasiyanik/bsu/coursework/tests/DiplomaTest.java
@@ -4,45 +4,67 @@ import com.akasiyanik.bsu.coursework.PoissonSolver;
 import com.akasiyanik.bsu.coursework.PoissonSolverWithClarification;
 import com.akasiyanik.bsu.coursework.PoissonSolverWithOpression;
 import com.akasiyanik.bsu.coursework.PoissonSolverWithOpressionAndClarification;
+import org.apache.commons.lang3.time.StopWatch;
 
 /**
  * @author Aliaksei Kasiyanik
  */
 public class DiplomaTest {
 
-    private static double eps = Math.pow(10, -6);
+    private static double eps = Math.pow(10, -8);
 
     public static void main(String[] args) {
         double tau = 0.05;
-        int n = 20;
+        int[] testSizes = {20, 30, 50, 100, 250};
 
         double t0 = 0.0;
 
+        for (int n : testSizes) {
+            double[] y0 = getY0(n);
+            System.out.println("-------- SIZE N = " + n + "-----------");
+
+            System.out.println("--- SP ---");
+
+            StopWatch w = new StopWatch();
+            w.start();
+            PoissonSolver solver = new PoissonSolver();
+            solver.solve(t0, y0, tau, eps);
+            w.split();
+            System.out.println("Time : " + w.toSplitString());
+            w.stop();
+            w.reset();
+
+            System.out.println("--- SP with CLARIFICATION ---");
+
+            w.start();
+            PoissonSolverWithClarification solverWithClarif = new PoissonSolverWithClarification();
+            solverWithClarif.solve(t0, y0, tau, eps);
+            w.split();
+            System.out.println("Time : " + w.toSplitString());
+            w.stop();
+            w.reset();
+
+            System.out.println("--- SP with PRECONDITIONING ---");
+
+            w.start();
+            PoissonSolverWithOpression solverWithOpr = new PoissonSolverWithOpression();
+            solverWithOpr.solve(t0, y0, tau, eps);
+            w.split();
+            System.out.println("Time : " + w.toSplitString());
+            w.stop();
+            w.reset();
+
+//        PoissonSolverWithOpressionAndClarification superSolver = new PoissonSolverWithOpressionAndClarification();
+//        superSolver.solve(t0, y0, tau, eps);
+        }
+
+    }
+
+    private static double[] getY0(int n) {
         double[] y0 = new double[n];
         for (int i = 0; i < n; i++) {
             y0[i] = 1.0;
         }
-
-
-        System.out.println("--- SP ---");
-
-        PoissonSolver solver = new PoissonSolver();
-        solver.solve(t0, y0, tau, eps);
-
-
-        System.out.println("--- SP with CLARIFICATION ---");
-
-        PoissonSolverWithClarification solverWithClarif = new PoissonSolverWithClarification();
-        solverWithClarif.solve(t0, y0, tau, eps);
-
-
-        System.out.println("--- SP with PRECONDITIONING ---");
-
-        PoissonSolverWithOpression solverWithOpr = new PoissonSolverWithOpression();
-        solverWithOpr.solve(t0, y0, tau, eps);
-
-//        PoissonSolverWithOpressionAndClarification superSolver = new PoissonSolverWithOpressionAndClarification();
-//        superSolver.solve(t0, y0, tau, eps);
-
+        return y0;
     }
 }
